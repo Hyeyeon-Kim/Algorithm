c0ae772c92e48f0ba31dab984b3bbb7303b4455c
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2024-08-08 11:04:45 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1896.55 ms, Memory: 175 MB -BaekjoonHub

---
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<vector<int>> edges) {
+    map<int,vector<int>> input;  //in, out
+    int root;
+    vector <int> answer(4, 0);
+    
+    for (auto edge : edges){
+        if (input.find(edge[0]) == input.end())
+            input.insert({edge[0], {0, 1}});
+        else
+            input[edge[0]][1] += 1;
+        
+        
+        if (input.find(edge[1]) == input.end())
+            input.insert({edge[1], {1, 0}});
+        else
+            input[edge[1]][0] += 1;
+    }
+    
+    int shapeCnt = 0;
+    
+    for (auto a: input){
+        if (a.second[0] == 0 && a.second[1] >= 2){
+            root = a.first;
+            shapeCnt = input[root][1];
+            input.erase(root);
+            answer[0] = root;
+        }
+    }
+    
+    for (auto edge : edges){
+        if (edge[0] == root)
+            input[edge[1]][0]--;
+    }
+    
+    //막대 갯수
+    for (auto a: input){
+        if (a.second[0] == 0)
+            answer[2]++;
+    }
+    
+    //8자 갯수
+    for (auto a: input){
+        if (a.second[0] == 2 && a.second[1] == 2)
+            answer[3]++;
+    } 
+    answer[1] = shapeCnt - (answer[2] + answer[3]);
+    
+    cout << shapeCnt;
+        // cout << a.first << ": " << a.second[0] << " " << a.second[1] << endl;
+    
+    // cout << root;
+    
+//     vector <vector<int> > node(input.size(), vector<int>(2, 0));
+    
+//     for (auto n : input){
+//         vector 
+//     }
+    return answer;
+}
