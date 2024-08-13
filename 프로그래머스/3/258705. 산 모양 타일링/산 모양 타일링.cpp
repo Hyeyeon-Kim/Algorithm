25ff1ed12e6c7f1f578157ca553bcf84b2fe6bc2
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2024-08-13 20:28:19 +0900

    [level 3] Title: 산 모양 타일링, Time: 1.08 ms, Memory: 6.77 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int solution(int n, vector<int> tops) {
+    vector<int> v(n + 1);
+    v[0] = 1;
+    v[1] = 3 + tops[0];
+    
+    for (int i = 2; i <= n; i++) {
+        v[i] = (v[i-1] * (3 + tops[i-1]) - v[i-2]) % 10007;
+        
+        if (v[i] < 0) 
+        	v[i] += 10007;  // 음수를 방지
+    }
+
+    return v[n];
+} 
