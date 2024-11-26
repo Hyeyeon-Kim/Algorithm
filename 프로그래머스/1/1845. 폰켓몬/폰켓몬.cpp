e16ef866d88738e8c38eb77cc5daaebbb040f38d
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2024-11-26 16:13:27 +0900

    [level 1] Title: 폰켓몬, Time: 1.20 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <set>
+// #include <iostream>
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    set<int> s;
+    
+    for (int num : nums)
+        s.insert(num);
+    
+    if (s.size() > nums.size()/2)
+        return nums.size()/2;
+    else
+        return s.size();
+}
