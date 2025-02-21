02fb4220efb58e55f1d1e685042a1088268a814f
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-02-21 17:08:07 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 168 ms, Memory: 75748 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -1,86 +1,93 @@
 #include <iostream>
-#include <vector>
 using ll = long long;
 using namespace std;
 
-int  n, m, k;
-ll tree[1 << 22] = {0};
+ll tree[1 << 22];
 ll lazy[1 << 22] = {0};
-ll arr[1 << 20];
+ll num[1 << 20];
 
 ll init(int node, int start, int end){
-    if (start == end){
-        tree[node] = arr[start];
-        return tree[node];
-    }
-
-    int mid = (start + end) / 2;
-    tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
-    return tree[node];
-}
-
-void lazy_update(int start, int end, int node){
-    if (lazy[node] == 0)
-		return;
-
-    tree[node] += (end - start + 1) * lazy[node];
-	if (start != end)
-	{
-		lazy[node * 2] += lazy[node];
-		lazy[node * 2 + 1] += lazy[node];
-	}
-	lazy[node] = 0;
-}
-
-ll sum(int node, int start, int end, int left, int right){
-    lazy_update(start, end, node);
-
-    if (end < left || start > right)
-        return 0;
+    if (start == end)
+        return tree[node] = num[start];
     
+    int mid = (start + end) / 2;
+    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+}
+
+void lazyUpdate (int node, int start, int end){
+    if (lazy[node] == 0)
+        return;
+    
+    tree[node] += (end - start + 1) * lazy[node];
+
+    if (start != end){
+        lazy[node * 2] += lazy[node];
+        lazy[node * 2 + 1] += lazy[node];
+    }
+    lazy[node] = 0;
+}
+
+ll sum (int node, int start, int end, int left, int right){
+    lazyUpdate(node, start, end);
+
+    if (left > end || right < start)
+        return 0;
+
     if (left <= start && right >= end)
         return tree[node];
 
     int mid = (start + end) / 2;
+
     return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid + 1, end, left, right);
 }
 
-void update(int node, int start, int end, ll cal, int left, int right){
-    lazy_update(start, end, node);
-    
-    if (end < left || start > right)
+void update(int node, int start, int end, int left, int right, long long d){
+    lazyUpdate(node, start, end);
+
+    if (left > end || right < start)
         return ;
 
-    if (left <= start && right >= end){
-        tree[node] += (end - start + 1) * cal;
-		if (start != end)
-		{
-			lazy[node * 2] += cal;
-			lazy[node * 2 + 1] += cal;
-		}
-		return;
-    }
 
+    if (left <= start && right >= end){
+        tree[node] += (end - start + 1) * d;
+        
+        if (start != end){
+            lazy[node * 2] += d;
+            lazy[node * 2 + 1] += d;
+        }
+        return ;
+    }
+    
     int mid = (start + end) / 2;
-    update(node * 2, start, mid, cal, left, right);
-    update(node * 2 + 1, mid + 1, end, cal, left, right);
-    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    
+    update(node * 2, start, mid, left, right, d);
+    update(node * 2 + 1, mid + 1, end, left, right, d);
+
+    tree[node] = tree[node * 2] + tree[node * 2 + 1]; 
+}
+
+void print(int n){
+    for (int i = 1; i <= n * 4; i++){
+        cout << tree[i] << " ";
+    } 
+    cout << endl;
 }
 
 int main(){
-    ios::sync_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, m, k;
 
     cin >> n >> m >> k;
-
-    for (int i = 1; i <= n; i++){
-        cin >> arr[i];
-    }
-
+    
+    for (int i = 1; i <= n; i++)
+        cin >> num[i];
+    
     init(1, 1, n);
-
-    for (int i = 0; i < m + k; i++){
+    // print(n);
+    for (int i = 0; i < m +k; i++){
         int a, b, c;
         ll d;
 
@@ -88,12 +95,12 @@ int main(){
 
         if (a == 1){
             cin >> b >> c >> d;
-            update (1, 1, n, d, b, c);
+            update(1, 1, n, b, c, d);
         }
         else{
             cin >> b >> c;
-            cout << sum(1, 1, n, b, c) << '\n';
-        }
-    }   
-
+            cout << sum(1, 1, n, b, c) << "\n";
+        }   
+        // print(n);
+    }
 }
