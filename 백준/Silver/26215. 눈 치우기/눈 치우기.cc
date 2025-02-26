d279925780395aa79f3431cf7dcd859a4fde987b
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-02-26 22:32:53 +0900

    [Silver III] Title: 눈 치우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/26215. 눈 치우기/눈 치우기.cc
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.cc
@@ -4,30 +4,47 @@
 using namespace std;
 
 int n;
-vector<int> arr(n, 0);
+priority_queue<int> pq;
+
+int solve(){
+
+	int ans = 0;
+
+	while (!pq.empty()){
+		int a = pq.top();
+		pq.pop();
+		
+		int b;
+		
+		if (!pq.empty()){
+			b = pq.top();
+			pq.pop();
+			if (b > 1)
+				pq.push(--b);
+		}
+		if (a > 1)
+			pq.push(--a);
+
+		ans++;
+
+		if (ans > 1440)
+			return -1;
+	}
+	return ans;
+}
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    
-    cin >> n;
-    arr = vector<int> (n, 0);
-    
-    long long total = 0;
-    int maxTime = 0;
-    
-    for (int i = 0; i < n; i++){
-        cin >> arr[i];
-        total += arr[i];
-        maxTime = max(maxTime, arr[i]);
-    }
-    
-    int requiredTime = max(maxTime, (int)((total + 1) / 2));
-    
-    if (requiredTime > 1440)
-        cout << -1;
-    else
-        cout << requiredTime;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++){
+		int num;
+		cin >> num;
+		pq.push(num);
+	}
+
+	cout << solve();
 }   
