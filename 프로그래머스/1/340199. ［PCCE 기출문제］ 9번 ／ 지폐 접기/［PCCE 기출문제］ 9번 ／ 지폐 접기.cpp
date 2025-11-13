e1907c34ca63b75922c7611b31fb20ebaeebc158
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-11-13 19:12:51 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -1,37 +1,9 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
-
-// 무조건 h가 작다고 가정 가로, 세로
-
-void swap(vector<int> &obj){
-    if (obj[0] >= obj[1])
-        return ;
-    int tmp = obj[0];
-    obj[0] = obj[1];
-    obj[1] = tmp;
-}
-
-bool is_done(int w_w, int w_h, int b_w, int b_h){
-    if (b_h <= w_h && b_w <= w_w)
-        return true;
-    return false;
-}
 
 int solution(vector<int> wallet, vector<int> bill) {
     int answer = 0;
-    // cout << tmp[0] << " " << tmp[1];
-    swap(wallet);
-    swap(bill);
-    
-    while (is_done(wallet[0], wallet[1], bill[0], bill[1]) == 0){
-        bill[0] /= 2;
-        swap(wallet);
-        swap(bill);
-        answer++;
-        cout << wallet[0] << " " << wallet[1] << " " << bill[0] << " " <<bill[1]<< "\n";
-    }
-    
     return answer;
 }
