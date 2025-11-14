e0068c09da1d89939782126660589bff3ccec977
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-11-14 16:39:20 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 42.25 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,40 +1,49 @@
+class user:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.plus = []
+        self.minus = []
+
+user_idx = []
+all_user = []
+
+def find_idx(name):
+    if (name not in user_idx):
+        user_idx.append(name)
+        all_user.append(user(name))
+    return user_idx.index(name)
+
+def reco(g, t):
+    all_user[t].plus[g] += 1
+    all_user[t].score -= 1
+    all_user[g].minus[t] += 1  
+    all_user[g].score += 1
+    
 def solution(friends, gifts):
+    n = len(friends)
+    for friend in friends:
+        i = find_idx(friend)
+        all_user[i].plus = [0] * n 
+        all_user[i].minus = [0] * n 
     
-    log = {}
-    give = {}
-    take = {}
-    next = {}
-    
-    for fri in friends:
-        give[fri] = 0
-        take[fri] = 0
-        next[fri] = 0
-        log[fri] = {}
-        for fr in friends:
-            log[fri][fr] = 0
-        
- 
-    # print(log)
-
     for gift in gifts:
-        to, fro = gift.split(' ')
-        log[to][fro] += 1
-        give[to] += 1
-        take[fro] += 1
+        give, take = gift.split(' ')
+        g = find_idx(give)
+        t = find_idx(take)
+        reco(g, t)
     
-
-    
-    
-    for fri in friends:
-        for fr in friends:
-            if fri == fr:
+    next = [0] * n 
+    i = 0 
+    for u in all_user:
+        cnt = 0
+        for j in range(n):
+            if i == j:
                 continue
-            
-            if log[fri][fr] > log[fr][fri]:
-                next[fri] += 1
-            elif log[fri][fr] == log[fr][fri]:
-                if give[fri]-take[fri] > give[fr]-take[fr]:
-                    next[fri] += 1
+            if all_user[i].minus[j] > all_user[j].minus[i]:
+                next[i] += 1
+            elif all_user[i].minus[j] == all_user[j].minus[i] and all_user[i].score > all_user[j].score:
+                next[i] += 1
+        i += 1
     
-
-    return next[max(next, key = next.get)]
+    return max(next)
