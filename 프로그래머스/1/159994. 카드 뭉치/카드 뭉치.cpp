a93bb2c1be603531decb2389e45df03f005a4a8c
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-09 13:01:36 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -1,33 +1,19 @@
 #include <string>
 #include <vector>
-#include <stack>
+#include <deque>
 #include <iostream>
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    
-    stack<string> card1; 
-    
-    for (int  i = cards1.size()-1; i >= 0; i--)
-        card1.push(cards1[i]);
-    
-    stack<string> card2; 
-    
-    for (int  i = cards2.size()-1; i >= 0; i--)
-        card2.push(cards2[i]);
-    
-    // while(!card1.empty()){
-    //     string s = card1.top();
-    //     cout << s <<  " ";
-    //     card1.pop();
-    // }
-    
+    deque<string> card1(cards1.begin(), cards1.end()); 
+    deque<string> card2(cards2.begin(), cards2.end()); 
+
     for (string s: goal){
-        if (!card1.empty() && s == card1.top()){
-            card1.pop();
+        if (!card1.empty() && s == card1.front()){
+            card1.pop_front();
         }
-        else if (!card2.empty() && s == card2.top())
-            card2.pop();
+        else if (!card2.empty() && s == card2.front())
+            card2.pop_front();
         else
             return "No";
     }
