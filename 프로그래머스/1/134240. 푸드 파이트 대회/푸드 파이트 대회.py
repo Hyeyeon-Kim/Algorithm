540570958dfbf51c83022c98bac4377ab434f469
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-12 20:21:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,38 +1,10 @@
 def solution(food):
-    str_len = 1
+    
+    s = []
     
     for i in range(1, len(food)):
-        if (food[i] % 2 != 0) :
-            food[i] -= 1
-        str_len += food[i]
-    
-    result = [0] * str_len
-    
-    s = 0;
-    e = str_len - 1;
-    typ = 1;
-    cnt = 0
-    
-    print(food)
-    print(len(result))
-                   
-    while (s <= e):
-        if (s == e):
-            result[s] = 0
-            break
+        cnt = food[i] // 2
+        s.append(str(i) * cnt)
             
-        for i in range(food[typ] // 2):
-            result[s] = typ
-            result[e] = typ
-            s += 1
-            e -= 1
-
-        typ += 1 
-    
-    answer = ""
-    
-    for n in result:
-        answer += str(n)
-        
-    print(result)
-    return answer
+    answer = "".join(s)
+    return answer + "0" + answer[::-1]
