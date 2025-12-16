fe4cf4acf16936f75b3d76d5cb113daf9c004230
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-16 12:43:05 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -1,18 +1,8 @@
 #include <string>
 #include <vector>
-
+#include <numeric>
 using namespace std;
 
 int solution(vector<int> numbers) {
-    int num[10] = {0};
-    int answer = 0;
-    
-    for (int n: numbers){
-        num[n] = 1;
-    }
-    
-    for (int i = 0; i < 10; i++)
-        if (num[i] == 0)
-            answer += i;
-    return answer;
+    return 45 - accumulate(numbers.begin(), numbers.end(), 0);
 }
