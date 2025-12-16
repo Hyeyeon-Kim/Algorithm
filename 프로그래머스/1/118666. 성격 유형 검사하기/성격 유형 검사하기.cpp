93f0685bd57d96a74bd1b241c5c4f9514bf78376
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-16 12:08:45 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.12 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.cpp
@@ -4,43 +4,32 @@
 #include <map>
 using namespace std;
 
-vector<int> mbti(26); 
+// vector<int> mbti(26);
 
-string find_idx(int i){
-    if (i == 0)
-        return "RT";
-    else if (i == 1) 
-        return "CF";
-    else if (i == 2)
-        return "JM";
-    else
-        return "AN";
-}
+char mbti[4][2] = {{'R','T'},{'C','F'},{'J','M'},{'A','N'}};
 
 string solution(vector<string> survey, vector<int> choices) {
+    map<char, int> score;
     
     for (int i = 0; i < survey.size(); i++)
     {
         string type = survey[i];
         
-        int ignore = type[0] - 'A';
-        int agree = type[1] - 'A';
-        
         if (choices[i] < 5)
-            mbti[ignore] += (4 - choices[i]);
+            score[type[0]] += (4 - choices[i]);
         else
-            mbti[agree] += choices[i] % 4;
+            score[type[1]] += (choices[i] % 4);
     }
     
         string answer = "";
     
         for (int i = 0; i < 4; i++){
-        string type = find_idx(i);
-        cout << type << "\n" <<  mbti[type[0]- 'A'] << " " << mbti[type[1] - 'A'] << "\n";
-        if (mbti[type[0] - 'A'] < mbti[type[1] - 'A'])
-            answer += type[1];
-        else
-            answer += type[0];
+            auto type = mbti[i];
+
+            if (score[type[0]] < score[type[1]])
+                answer += type[1];
+            else
+                answer += type[0];
             
         }
         cout << endl;
