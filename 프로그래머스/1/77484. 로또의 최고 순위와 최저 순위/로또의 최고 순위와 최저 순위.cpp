a5d864d2d7b137f3ec15874fd79e4badd7cb83ce
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-18 14:07:10 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    int hit = 0;
+    int blank = 0;
+    int arr[45] = {0};
+    
+    for (int i = 0; i < 6; i++){
+        if (lottos[i] == 0)
+            blank++;
+        arr[lottos[i]] = 1;
+    }
+    
+    for (int i = 0; i < 6; i++){
+        if (arr[win_nums[i]] == 1)
+            hit++;
+    }
+    
+    int worst;
+    
+    if (hit <= 1)
+        worst = 6;
+    else
+        worst = 7 - hit;
+        
+    int best;
+    
+    if (hit + blank <= 1)
+        best = 6;
+    else
+        best = 7 - hit - blank;
+    
+    return {best, worst};
+}
