c12e76afe94cbf7e89e3d496be67da7cb2582595
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-18 20:58:08 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.36 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,38 +1,19 @@
-import sys, math
 import re
-from fractions import Fraction
 
 def solution(new_id):
     new_id = new_id.lower()
-    s = '~!@#$%^&*()=+[{]}:?,<>/'
-    for i in s:
-        new_id = new_id.replace(i,'')
     
-    ids = new_id.split('.')
-    nids = []
-    for i in ids:
-        if i != '':
-            nids.append(i)
-            
-    new_id = '.'.join(nids)
-    
-    if new_id != '' and new_id[0] == '.':
-        new_id = new_id[1:]
-
-    if new_id != '' and new_id[len(new_id)-1] == '.':
-        new_id = new_id[0:len(new_id)-2]
+    new_id = re.sub('[^a-z0-9\-_.]','',new_id)
+    new_id = re.sub('\.{2,}','.', new_id)
+    new_id = re.sub('^\.|\.$', '', new_id)
     
     if new_id == '':
         new_id = 'a'
 
-    if len(new_id) >= 16:
-        new_id = new_id[:15]
-    
-    if new_id != '' and new_id[len(new_id)-1] == '.':
-        new_id = new_id[0:len(new_id)-1]
+    new_id = new_id[:15]
+    new_id = re.sub('\.$','', new_id)
     
     while len(new_id) < 3:
-        c = new_id[::-1]
-        new_id += c[:1]
+        new_id += new_id[-1]
     
     return new_id
