e9fbb851365a11b00c7caa8e4e43c38e92514f46
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-22 22:51:18 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.23 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,53 +1,25 @@
 import re
 
 def solution(dartResult):
-    score = []
-    bonus = []
-    option = []
-    
-    n = -1
-    
-    pots = re.split(r'([A-Z*#])', dartResult)
-    
-    for pot in pots:
-        if pot == '':
-            continue
-        elif pot == '*' or pot == '#':
-            option[n] = pot
-        elif pot == 'S' or pot == 'D' or pot == 'T':
-            bonus.append(pot)
-        else:
-            score.append(pot)
-            option.append(0)
-            n += 1
-    
+    pattern = re.findall(r'(\d+)([SDT])([*#]?)',dartResult)
+
     answer = 0
-    pre = 0
+    scores = []
     
-    for i in range(n+1):
-        print(score[i], bonus[i], option[i], '  ')
-        s = int(score[i])
-    
-        if (bonus[i] == 'D'):
+    for i, (num, bonus, option) in enumerate(pattern):
+        s = int(num)
+        
+        if (bonus == 'D'):
             s **= 2
-        elif (bonus[i] == 'T'):
+        elif (bonus == 'T'):
             s **= 3
             
-        if (option[i] == '*'):
-            pre *= 2
+        if (option == '*'):
+            if scores: 
+                scores[-1] *= 2
             s *= 2
-        elif (option[i] == '#'):
+        elif (option == '#'):
             s = -s
+        scores.append(s)
         
-        print(answer, pre)
-        answer += pre
-        pre = s
-        
-    answer += pre
-    print(answer, pre)
-    # print(score)
-    # print(bonus)
-    # print(option)
-    
-
-    return answer
+    return sum(scores)
