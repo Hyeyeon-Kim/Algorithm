b21d2fcef1cde80d0be60cdd7457f6c216c617c1
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-25 22:19:59 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    answer.insert(0, phone_number.size()-4, '*');
+    
+    for (int i = phone_number.size()-4; i < phone_number.size(); i++)
+        answer += phone_number[i];
+    return answer;
+}
