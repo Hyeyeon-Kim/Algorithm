3dbf3d13bb53f4f8c955f356c4785926c41e6391
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-26 22:11:08 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int gcd(int a, int b){
+    while (b != 0){
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+vector<int> solution(int n, int m) {
+    int g = gcd(n, m);
+    int l = n / g * m;
+    return {g, l};
+}
