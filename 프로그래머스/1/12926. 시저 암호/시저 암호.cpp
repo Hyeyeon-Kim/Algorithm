d621cb29f8a42b5a17a826b4a8982777ff2114b9
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2025-12-30 20:46:26 +0900

    [level 1] Title: 시저 암호, Time: 0.10 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for (char c : s){
+        if (c >= 'A' && c <= 'Z')
+            c = ((c - 'A') + n) % 26 + 'A';
+        else if (c >= 'a' && c <= 'z')
+            c = ((c - 'a') + n) % 26 + 'a';
+        answer += c;
+    }
+    return answer;
+}
