71fb138f68c7c929ee81715982037511191cbaa6
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-02 17:49:22 +0900

    [level 1] Title: 유연근무제, Time: 2.20 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/README.md
+++ 프로그래머스/1/388351. 유연근무제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 유연근무제 - 388351 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/388351) 
 
 ### 성능 요약
 
-메모리: 3.89 MB, 시간: 0.17 ms
+메모리: 4.16 MB, 시간: 2.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 10일 20:47:27
+2026년 01월 02일 17:49:19
 
 ### 문제 설명
 
--- 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cpp
@@ -4,78 +4,36 @@
 using namespace std;
 
 int solution(vector<int> schedules, vector<vector<int>> timelogs, int startday) {
-    int n = schedules.size();
     
-    // int weekend_s = (6 - startday) < 0 ? 6 : 6 - startday;
-    
-    // cout << startday << " " << weekend_s<< endl;
-        
-    int answer = 0;
-    
-    for (int i = 0; i < n; i++){
-        
-        // int sh, sm, hp, mp, cnt = 0;
-        bool ok = true;
-        
-        // sh = schedules[i] / 100;
-        // sm = schedules[i] % 100; 
-        // mp = (sm + 10) % 60;
-        // hp = sh + (sm + 10) / 60;
-        
-        // cout << sh << ":" << sm << " ~ " << hp << ":" << mp << "     ";
-        
-        for (int j = 0; j < 7; j++){
-            int day = (startday + j - 1) % 7 + 1; // j번째 날의 실제 요일 (1~7)
-            if (day == 6 || day == 7) 
-                continue;
-            
-           int limit = (schedules[i] / 100) * 60 + (schedules[i] % 100) + 10;
-            int logtime = (timelogs[i][j] / 100) * 60 + (timelogs[i][j] % 100);
-
-            if (logtime > limit) { // 지각
-                ok = 0;
-                break;
-            }
-            
-//             int h, m;
-            
-//             h = timelogs[i][j] / 100;
-//             m = timelogs[i][j] % 100;
-            
-//             // cout << h << ":" << m << "     ";
-            
-//             if (sh > h){
-//                 cout << h << ":" << m << "     ";
-//                 cnt++;
-//             }
-//             else if (sh == h){
-//                 if (m <= mp){
-//                     cout << h << ":" << m << "     ";
-//                     cnt++;
-//                 }
-//             }            
-//             else if (sh < h && hp == h){
-//                 if (sm >= m && m <= mp){
-//                     cout << h << ":" << m << "     ";
-//                     cnt++;
-//                 }
-//             }
-//         }     
-        
-        }
-        if (ok)
-            answer++;
+    for (int i = 0; i < schedules.size(); i++){
+        int h = schedules[i] / 100;
+        int m = schedules[i] % 100;
+        schedules[i] = h * 60 + m + 10;
+        cout << schedules[i] << " ";
     }
     
-    return answer;
-}
+    cout << endl;
+    
+    int ans = 0;
+    
+    for (int i = 0; i < timelogs.size(); i++){
+        bool give = true; 
+        for (int j = 0 ; j < 7; j++){
+            if ((j + startday) % 7 == 0 ||  (j + startday) % 7 == 6)
+                continue;
 
-/*
-1 5
-2 4
-3 3
-4 2
-5 1
-6 0
-7 6
-*/
+            int h = timelogs[i][j] / 100;
+            int m = timelogs[i][j] % 100;
+            int time = h * 60 + m;
+            if (time > schedules[i])
+                give = false; 
+            cout << time << " ";
+        }
+        cout << give;
+        if (give)
+            ans++;
+        cout << endl;
+    }
+    
+    return ans;
+}
