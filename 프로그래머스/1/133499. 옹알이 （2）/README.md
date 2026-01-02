97301f57033da43b50259f51f45fa16bc33b8aa0
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-02 20:16:30 +0900

    [level 1] Title: 옹알이 (2), Time: 0.46 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/README.md
+++ 프로그래머스/1/133499. 옹알이 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 옹알이 (2) - 133499 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133499) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.19 ms
+메모리: 9.1 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 12일 22:49:44
+2026년 01월 02일 20:16:28
 
 ### 문제 설명
 
--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,30 +1,24 @@
+import re
+
 def solution(babbling):
-    can = ["aya","ye","woo","ma"]
-    ans = 0
+    cnt = 0
     
-    
-    for bal in babbling:
-        i = 0
-        pre = ""
+    for s in babbling:
         can = True
-        while (i < len(bal)):
-            # print(bal[i:i+3], bal[i:i+2] )
-            if (bal[i:i+3] == "aya" and pre != "aya"):
-                pre = "aya"
-                i += 3
-            elif (bal[i:i+2] == "ye"and pre != "ye"):
-                pre = "ye"
-                i += 2
-            elif (bal[i:i+3] == "woo"and pre != "woo"):
-                pre = "woo"
-                i += 3
-            elif (bal[i:i+2] == "ma"and pre != "ma"):
-                pre = "ma"
-                i += 2
+        pre = ''
+        
+        while len(s):
+            if s[:3] != pre and (s[:3] == "aya" or s[:3] == "woo"):
+                pre = s[:3]
+                s = s[3:]
+            elif s[:2] != pre and (s[:2] == "ye" or s[:2] == "ma"):
+                pre = s[:2]
+                s = s[2:]   
             else:
                 can = False
                 break
         
         if can:
-            ans += 1
-    return ans
+            cnt += 1
+                
+    return cnt
