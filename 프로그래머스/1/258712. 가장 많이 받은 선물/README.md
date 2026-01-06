35d0c5084f7c5a200d101813b7fe235cb01586e7
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-06 18:22:23 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 10.21 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 9.93 MB, 시간: 42.25 ms
+메모리: 9.68 MB, 시간: 10.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 14일 16:39:18
+2026년 01월 06일 18:22:20
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,49 +1,33 @@
-class user:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.plus = []
-        self.minus = []
-
-user_idx = []
-all_user = []
-
-def find_idx(name):
-    if (name not in user_idx):
-        user_idx.append(name)
-        all_user.append(user(name))
-    return user_idx.index(name)
-
-def reco(g, t):
-    all_user[t].plus[g] += 1
-    all_user[t].score -= 1
-    all_user[g].minus[t] += 1  
-    all_user[g].score += 1
-    
 def solution(friends, gifts):
-    n = len(friends)
+    state = {}
+    give = {}
+    take = {}
+    result = {}
+    
     for friend in friends:
-        i = find_idx(friend)
-        all_user[i].plus = [0] * n 
-        all_user[i].minus = [0] * n 
-    
+        state[friend] = {f: 0 for f in friends}
+        give[friend] = 0
+        take[friend] = 0
+        result[friend] = 0
+        
     for gift in gifts:
-        give, take = gift.split(' ')
-        g = find_idx(give)
-        t = find_idx(take)
-        reco(g, t)
+        g, t = gift.split(' ')
+        state[g][t] += 1
+        give[g] += 1
+        take[t] += 1
+        
     
-    next = [0] * n 
-    i = 0 
-    for u in all_user:
+    for friend in friends:
         cnt = 0
-        for j in range(n):
-            if i == j:
+        for author in friends:
+            if friend == author:
                 continue
-            if all_user[i].minus[j] > all_user[j].minus[i]:
-                next[i] += 1
-            elif all_user[i].minus[j] == all_user[j].minus[i] and all_user[i].score > all_user[j].score:
-                next[i] += 1
-        i += 1
+            
+            if state[friend][author] > state[author][friend]:
+                cnt += 1
+            elif state[friend][author] == state[author][friend] and give[friend] - take[friend] > give[author] - take[author]:
+                cnt += 1
+        
+        result[friend] = cnt
     
-    return max(next)
+    return max(result.values())
