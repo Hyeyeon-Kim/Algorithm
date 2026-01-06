783df59e2a799f5709909fbc6030eff1106798a3
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-06 17:48:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 동영상 재생기 - 340213 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.09 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 12일 13:56:13
+2026년 01월 06일 17:48:22
 
 ### 문제 설명
 
--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.cpp
@@ -3,77 +3,52 @@
 #include <iostream>
 using namespace std;
 
-int ctoi(char c){
-    return c - '0';
+int change(string s){
+    return ((s[0] - '0') * 10 + (s[1] - '0')) * 60 + (s[3] - '0') * 10 + (s[4] - '0');
 }
 
-int minute(string clock){
-    int s =  (ctoi(clock[0]) * 10 + ctoi(clock[1])) * 60 + (ctoi(clock[3]) * 10 + ctoi(clock[4]));
-    return s;
-}
-
-string itos(int cur){
-    string str = "00:00";
+string re(int num){
+    string ss = "     ";
+    int m = num / 60;
+    int s = num % 60;
     
-    int m = cur / 60;
-    int s = cur % 60;
-    
-    str[0] = (m / 10) + '0';
-    str[1] = (m % 10) + '0';
-    str[3] = (s / 10) + '0';
-    str[4] = (s % 10) + '0';
-    return str; 
-}
-
-int pre_move(int pos){
-    if (pos < 10)
-        return 0;
-    else 
-        return pos - 10;
-}
-
-int next_move(int pos, int video_len){
-    if (pos + 10 > video_len)
-        return video_len;
-    else 
-        return pos + 10;
-}
-
-int skip(int pos, int op_start, int op_end){
-    // cout << op_start << ", " << op_end << "   ";
-    // cout << pos << ", "<< itos(pos) << "     ";
-    // cout << (op_start <= pos <= op_end) << "      ";
-
-    
-    if (op_start <= pos && pos <= op_end)
-        return op_end;
-    else
-        return pos;
+    ss[0] = m / 10 + '0';
+    ss[1] = m % 10 + '0';
+    ss[2] = ':';
+    ss[3] = s / 10 + '0';
+    ss[4] = s % 10 + '0';
+    return ss;
 }
 
 string solution(string video_len, string pos, string op_start, string op_end, vector<string> commands) {
-    int video = minute(video_len);
-    int cur = minute(pos);
-    int op_s = minute(op_start);
-    int op_e =  minute(op_end);
     
-    // cout << op_s << ", " << op_e << endl;
-    cur = skip(cur, op_s, op_e);
+    int cur = change(pos);
+    int end = change(video_len);
+    int os = change(op_start);
+    int oe = change(op_end);
     
-    for (int i = 0  ; i < commands.size(); i++){
-        
-        if (commands[i] == "next"){
-            cur = next_move(cur, video);
+    for (string c : commands){
+        if (cur >= os && cur <= oe){
+            cur = oe;
         }
-        else if (commands[i] == "prev"){
-            cur = pre_move(cur);
+            
+        if (c == "prev"){
+            if (cur - 10 <= 0)
+                cur = 0;
+            else
+                cur -= 10;
         }
-        
-         cur = skip(cur, op_s, op_e);
-        
-        // cout << cur << ", "<< itos(cur) << "     ";
-        cout << itos(cur) << endl;
+        else {
+            if (cur + 10 >= end)
+                cur = end;
+            else
+                cur += 10;
+        }
     }
     
-    return itos(cur);
+    if (cur >= os && cur <= oe){
+        cur = oe;
+    }
+    
+    return re(cur);
 }
