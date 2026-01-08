75a48c3d574c0a64c48c73b9658116f64205c8d6
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-08 23:19:35 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.24 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,47 +1,26 @@
-def day_count(date):
-    date = date.split(".")
-    
-    year = int(date[0]) * 28 * 12
-    mon = int(date[1]) * 28
-    day = int(date[2])
-    
-    today = year + mon + day
-    return today
+def strToDay(day):
+    y,m,d = map(int, day.split('.'))
+    return y * 336 + (m - 1) * 28 + (d - 1)
 
-def change_look(date):
-    ny = date // (28 * 12)
-    nm = (date % (28 * 12)) // 28
-    nd = (date % (28 * 12)) % 28
+def dayToStr(day):
+    y = day // 336
+    rem = day % 336
+    m = rem // 28
+    d = rem % 28
     
-    print(date, ny, nm , nd) 
+    return f"{y}.{m+1:02d}.{d+1:02d}"
 
 def solution(today, terms, privacies):
-    today = day_count(today)
-    change_look(today)
-    print()
-    
-    term = {}
-    
-    for tmp in terms:
-        tmp = tmp.split(" ")
-        term[tmp[0]] = int(tmp[1])
-    
-    # print(term)
+    today = strToDay(today)
+    terms = {t: int(d) for term in terms for t, d in [term.split(' ')]}
     
     answer = []
-    i = 1
-    for pri in privacies:
-        date, ty = pri.split(" ")
-        # print(date, ty)
-        
-        date = day_count(date)        
-        dead = date + (term[ty] * 28) - 1
-        
-        if dead < today:
-            answer.append(i)
-            
-        i += 1
-        change_look(dead)
-    
 
+    for i, privacie in enumerate(privacies, start = 1):
+        day, t = privacie.split(' ')
+        day = strToDay(day) + (terms[t] * 28) - 1
+        
+        if today > day:
+            answer.append(i)
+    
     return answer
