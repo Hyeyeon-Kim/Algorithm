5b79cef78a1f76de2b9430b27a02e2ca38214d32
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-15 15:51:30 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -4,35 +4,21 @@
 using namespace std;
 
 int solution(int n, int w, int num) {
-    // num = 1;
-    vector<vector<int>> box(n / w + 1, vector<int>(w));
+    int h = (num - 1) / w;      
     
-    for (int i = 0; i < n; i++){
-        
-        int ii = i / w;
-        int j = i % w;
-        
-        // cout << ii << " " << j << "   ";
-        if (ii % 2 == 0)
-            box[ii][w-1-j] = i+1;
-        else{
-            box[ii][j] = i+1;
-        }
-    }
-    
-    int h = (num-1) / w, ww;
-    if (h % 2 == 0)
-        ww = w - 1 - ((num-1) % w);
-    else
-        ww = (num-1) % w;;
-        
-   cout << h << " " << ww << "   \n";
     int cnt = 0;
-    
-    for (int i = h; i <= n / w; i++){
-        cout << box[i][ww] << " ";
-        if (box[i][ww])
-            cnt++;
-    }    
+    while (n >= num){
+        if (h % 2 == 0){
+            int col =  w - 1 - ((num - 1) % w);
+            num += col * 2 + 1;
+        }
+        else{
+            int col = (num - 1) % w;
+            num += (w - col) * 2 - 1;
+        }
+        cnt++;
+        // cout << num << " ";
+    }
+
     return cnt;
 }
