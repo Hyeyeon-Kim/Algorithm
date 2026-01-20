65741713625140b219c3445dc28534779e5d03ae
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-20 12:37:14 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.31 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,12 @@
+import re
+
 def solution(s):
-    
-    word = {"zero":"0","one":"1","two":"2","three":"3","four":"4","five":"5","six":"6","seven":"7","eight":"8","nine":"9"}
-    
-    for k, v in word.items():
-        s = s.replace(k, v)
-    return int(s)
+    mp = {
+        "zero":"0","one":"1","two":"2","three":"3","four":"4",
+        "five":"5","six":"6","seven":"7","eight":"8","nine":"9"
+    }
+    return int(re.sub(
+        "zero|one|two|three|four|five|six|seven|eight|nine",
+        lambda m: mp[m.group()],
+        s
+    ))
