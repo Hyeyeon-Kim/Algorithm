95b3ed68582dae87bee0b3fada2c63a1ce84b50b
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-22 10:26:58 +0900

    [level 1] Title: 햄버거 만들기, Time: 11.90 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -1,31 +1,32 @@
 #include <string>
 #include <vector>
-#include <deque>
-#include <list>
+#include <queue>
 #include <iostream>
 using namespace std;
 
+
+// 1 3 2 1 3 2 1 1 2
 int solution(vector<int> ingredient) {
-    vector<int> q;
-    
+    deque<int> de;
     int ans = 0;
     
-    for (int i = 0; i < ingredient.size(); i++){
-        q.push_back(ingredient[i]);
-        
-        int size = q.size();
-        
-        if (size < 4)
-            continue;
-        
-        if (q[size-1] == 1 && q[size-2] == 3 && q[size-3] == 2 && q[size-4] == 1){
-            ans++;
-            q.pop_back();
-            q.pop_back();
-            q.pop_back();
-            q.pop_back();   
+    for (int i: ingredient){
+        // if (i != 1 || de.size() < 4)
+        de.push_front(i);
+        if (de.size() >= 4) {
+            // cout << de[0] << " " << de[1] << " " << de[2] << " " << de[3] << "\n";
+            if (de[0] == 1 & de[1] == 3 & de[2] == 2 & de[3] == 1){
+                cout << "dd";
+                ans += 1;
+                for (int j = 0; j < 4; j++)
+                    de.pop_front();
+            }
         }
     }
-
+    
+    
+    // for (int a: de)
+    //     cout << a << " ";
+    
     return ans;
 }
