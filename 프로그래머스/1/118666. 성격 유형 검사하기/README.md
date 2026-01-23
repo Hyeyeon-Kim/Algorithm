91069ddee52268f297640a5ea258154810ffcd5d
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-23 16:31:20 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.36 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/README.md
+++ 프로그래머스/1/118666. 성격 유형 검사하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 성격 유형 검사하기 - 118666 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118666) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.43 ms
+메모리: 9.12 MB, 시간: 0.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 09일 16:28:32
+2026년 01월 23일 16:31:18
 
 ### 문제 설명
 
--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,25 +1,24 @@
 def solution(survey, choices):
-    mbti = {'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0}
+    state = {i: 0 for i in "RTCFJMAN"}
     
-    for i in range(0, len(survey)):
-        if choices[i] < 4:
-            mbti[survey[i][0]] += 4 - int(choices[i])
+    for i, s in enumerate(survey):
+        n, p = s[0], s[1]
+        score = choices[i]
         
-        elif (choices[i] > 4):
-            mbti[survey[i][1]] += int(choices[i]) - 4
-        
+        if (score < 4):
+            state[n] += 4 - score
+        else:
+            state[p] += score - 4
+    # print(state)
     answer = ['R','C','J','A']
     
-    if mbti['R'] < mbti['T']:
+    if state['T'] > state['R']:
         answer[0] = 'T'
-    if mbti['C'] < mbti['F']:
+    if state['F'] > state['C']:
         answer[1] = 'F'
-    if mbti['J'] < mbti['M']:
+    if state['M'] > state['J']:
         answer[2] = 'M'
-    if mbti['A'] < mbti['N']:
+    if state['N'] > state['A']:
         answer[3] = 'N'
-    print(mbti)
+        
     return ''.join(answer)
-
-#     if mbti['R'] < mbti['T']:
-        # answer[0] = 'T'
