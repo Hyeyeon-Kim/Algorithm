9350b18a37537f8e1088d9185a2d2684596986da
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-23 16:42:59 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.46 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -2,17 +2,17 @@ import re
 
 def solution(new_id):
     new_id = new_id.lower()
-    new_id = re.sub('[^0-9a-z-_.]','', new_id)
-    new_id = re.sub('\.+','.', new_id).strip('.')
+    new_id = re.sub('[^\w\d\-_\.]','', new_id)
+    new_id = re.sub('\.+', '.', new_id)
+    new_id = re.sub('^\.|\.$','', new_id)
     
     if new_id == '':
-        new_id = 'a'
-        
-    if len(new_id) >= 16:
-        new_id = new_id[:15].strip('.')
+        new_id += 'a'
     
-    if len(new_id) <= 2:
-        new_id += new_id[-1] * (3 - len(new_id))
+    new_id = new_id[:15]
+    new_id = re.sub('^\.|\.$','', new_id)
     
-    
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+
     return new_id
