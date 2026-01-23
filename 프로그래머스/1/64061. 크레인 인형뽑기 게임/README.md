ff17fe3ccab908c4e3e7c1ec0cb68e2a85e32dcd
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-23 17:22:55 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.55 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.44 ms
+메모리: 9.16 MB, 시간: 0.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 14일 14:28:49
+2026년 01월 23일 17:22:53
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,18 +1,30 @@
-from collections import deque
-
-def solution(board, moves):
-    board = [deque(x for x in col if x != 0) for col in zip(*board) ]
-    ans = 0 
+def solution(boards, moves):
+    boards = list(zip(*boards))
+    boards = [[b for b in board[::-1] if b != 0] for board in boards]
     
-    basket = deque()
-    for m in moves:
-        if board[m-1]:
-            now = board[m-1].popleft()
+    s = []
+    ans = 0
+    
+    print(boards)
+    
+    for move in moves:
+        tem = None
+        
+        if (boards[move - 1]):
+            tem = boards[move - 1][-1]
+            boards[move - 1].pop()
+        
+        if (tem == None):
+            continue
             
-            if basket and basket[-1] == now:
-                basket.pop();
+        if len(s):
+            if s[-1] == tem:
+                s.pop()
                 ans += 2
             else:
-                basket.append(now)
-            
+                s.append(tem)
+        else:
+            s.append(tem)
+        
+        # print(move, tem, s, '     ',boards)
     return ans
