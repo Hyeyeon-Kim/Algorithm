5c3ed93c87fd04e7025a8b926368c41f997d705a
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-26 17:36:21 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.02 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,27 +1,26 @@
 def solution(friends, gifts):
     
-    result = {f: {ff : 0 for ff in friends} for f in friends}
-    score = {f : 0 for f in friends}
+    state = {f: {ff: 0 for ff in friends if ff != f} for f in friends}
+    score = {f: 0 for f in friends}
     
     for gift in gifts:
-        give, take = gift.split(' ')
+        g, t = gift.split(' ')
         
-        result[give][take] += 1
+        state[g][t] += 1
+        score[g] += 1
+        score[t] -= 1
+    
+    next_ = []
+    
+    for g in friends:
+        cnt = 0
         
-        score[give] += 1
-        score[take] -= 1
-    
-    gift = [0 for i in friends]
-    
-    for i, friend in enumerate(friends):
-        for f in friends:
-            if f != friend:
-                # print(result[friend][f], result[f][friend])
-                if result[friend][f] > result[f][friend]:
-                    gift[i] += 1
-                    # print(1, score[friend], score[f], gift)
-                elif result[friend][f] == result[f][friend] and (score[friend] > score[f]):
-                    gift[i] += 1
-                    # print(2, score[friend], score[f], gift)
+        for t in state[g]:
+            if state[g][t] > state[t][g]:
+                cnt += 1
+            elif state[g][t] == state[t][g] and score[g] > score[t]:
+                cnt += 1
+                
+        next_.append(cnt)
 
-    return max(gift)
+    return max(next_)
