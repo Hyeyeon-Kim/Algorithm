0a12c5675e98348b0123033fdad7a1431d05e468
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-27 12:35:51 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.18 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,25 +1,22 @@
-from itertools import groupby
-
 def solution(mats, park):
-    
     n, m = len(park), len(park[0])
+    state = [[0] * (m + 1) for i in range(n+1)]
     
-    dp = [[0] * m for _ in range(n)]
-    max_len = 0
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if park[i - 1][j - 1] != '-1':
+                continue
+            state[i][j] = min(state[i-1][j], state[i][j-1], state[i-1][j-1]) + 1        
     
-    for i in range(n):
-        for j in range(m):
-             if park[i][j] == '-1':
-                if i == 0 and j == 0:
-                    dp[i][j] = 1
-                else:
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-                max_len = max(max_len, dp[i][j]) 
+    max_ = 0
+    for s in state:
+        if max(s) > max_:
+            max_ = max(s)
+        
+    mats = sorted(mats, reverse = True)
     
-    mats.sort(reverse=True)
-    
-    for x in mats:
-        if x <= max_len:
-            return x
-                
+    for m in mats:
+        if m <= max_:
+            return m
+        
     return -1
