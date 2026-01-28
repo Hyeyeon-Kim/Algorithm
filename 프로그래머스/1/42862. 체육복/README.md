e4812385c9a7b872dbaf74d3187e7259d198e42b
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-28 14:23:48 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 0.01 ms
+메모리: 9.25 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 20일 13:41:00
+2026년 01월 28일 14:23:46
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,20 +1,23 @@
 def solution(n, lost, reserve):
     ans = n - len(lost)
-    reserve = set(reserve)
-    lost = set(lost)
     
-    both = lost & reserve
-    lost -= both
-    reserve -= both
-    ans += len(both)
-    # print(lost)
-            
+    slost = set(lost)
+    reserve = set(reserve)
+    
+    for l in lost:
+        if l in reserve:
+            slost.remove(l)
+            reserve.remove(l)
+            ans += 1
+
+    lost = slost
+    
     for l in lost:
         if l - 1 in reserve:
-            ans += 1 
-            reserve.remove(l-1)
+            ans += 1
+            reserve.remove(l - 1)
         elif l + 1 in reserve:
-            ans += 1 
+            ans += 1
             reserve.remove(l+1)
     
     return ans
