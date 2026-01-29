dde3b7ebd94619c61f99f64dcdcd16a75703ec07
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-29 10:45:02 +0900

    [level 1] Title: 옹알이 (2), Time: 0.13 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,21 +1,27 @@
-import re
-
 def solution(babbling):
     ans = 0
     
     for bab in babbling:
-        b = re.split("(aya)|(ye)|(woo)|(ma)", bab)
-        token = list(filter(lambda x: x not in(None, ''), b))
-        
-        ok = True
         pre = ''
-        for t in token:
-            if pre == t or t not in ('aya','ye','woo','ma'):
+        ok = True    
+        
+        while(bab):
+            s2 = bab[:2]
+            s3 = bab[:3]
+            
+            if pre == s2 or pre == s3:
                 ok = False
                 break
-            pre = t
-        
+            
+            if s2 == 'ye' or s2 == 'ma': 
+                pre = s2
+                bab = bab[2:]
+            elif s3 == 'aya' or s3 == 'woo':
+                pre = s3
+                bab = bab[3:]
+            else:
+                ok = False
+                break
         if ok:
             ans += 1
-    
     return ans
