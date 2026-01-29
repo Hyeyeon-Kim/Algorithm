a70583c9c7bf94354ab3887fe33fa0ddd0325a32
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-29 10:54:49 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.05 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,6 +1,13 @@
-def change(t):
-    t = list(map(int, t.split(':')))
-    return t[0] * 60 + t[1]
+def change(time):
+    h, m = map(int, time.split(':'))
+    return h * 60 + m
+
+def re(time):
+    h = time // 60
+    m = time % 60
+    
+    
+    return str(h).zfill(2) + ':' + str(m).zfill(2)
 
 def solution(video_len, pos, op_start, op_end, commands):
     video_len = change(video_len)
@@ -12,16 +19,16 @@ def solution(video_len, pos, op_start, op_end, commands):
         if op_start <= pos <= op_end:
             pos = op_end
         
-        if c == "next":
+        if c == 'next':
             pos += 10
             if pos > video_len:
                 pos = video_len
-        elif c == "prev":
+        else:
             pos -= 10
             if pos < 0:
                 pos = 0
         
-    if op_start <= pos <= op_end:
-        pos = op_end
-
-    return str(pos//60).zfill(2) + ':' + str(pos%60).zfill(2)
+        if op_start <= pos <= op_end:
+            pos = op_end
+            
+    return re(pos)
