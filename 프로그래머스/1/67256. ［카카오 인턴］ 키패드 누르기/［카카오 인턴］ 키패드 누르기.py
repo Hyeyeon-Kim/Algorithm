5557676890fa093816dc9d39a996496a39b7fc74
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-01-29 11:03:01 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.36 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,38 +1,34 @@
 def solution(numbers, hand):
+    core = {i: [(i-1) // 3, (i-1) % 3] for i in range(1, 10)}
+    core[0] = [3, 1]
     
-    key = {i: [(i - 1) // 3, (i - 1) % 3] for i in range(1, 10)}
-    key[0] = [3, 1]
-
-    answer = ''
-    l = [3, 0] 
+    l = [3, 0]
     r = [3, 2]
+    ans = ''
     
     for num in numbers:
         if num in (1, 4, 7):
-            answer += 'L'
-            l = key[num]
+            l = core[num]
+            ans += 'L'
         elif num in (3, 6, 9):
-            answer += 'R'
-            r = key[num]
+            r = core[num]
+            ans += 'R'
         else:
-            ld = abs(l[0] - key[num][0]) + abs(l[1] - key[num][1])
-            rd = abs(r[0] - key[num][0]) + abs(r[1] - key[num][1])
+            Ldist = abs(core[num][0] - l[0]) + abs(core[num][1] - l[1])
+            Rdist = abs(core[num][0] - r[0]) + abs(core[num][1] - r[1])
             
-            if ld < rd:
-                answer += 'L'
-                l = key[num]
-            elif ld > rd:
-                answer += 'R'
-                r = key[num]
+            if Ldist < Rdist:
+                l = core[num]
+                ans += 'L'
+            elif Ldist > Rdist:
+                r = core[num]
+                ans += 'R'
             else:
-                if hand == 'left':
-                    answer += 'L'
-                    l = key[num]
+                if hand == 'right':
+                    r = core[num]
+                    ans += 'R'
                 else:
-                    answer += 'R'
-                    r = key[num]
-            
-        #     print(ld, rd, num)
-        # print(answer, num , l, r)
-    
-    return answer
+                    l = core[num]
+                    ans += 'L'
+                
+    return ans
