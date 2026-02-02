deb1c6b530f3ccb9e821198fb9fc2e3f00d0496a
Hyeyeon Kim <65168017+Hyeyeon-Kim@users.noreply.github.com>
2026-02-02 15:05:10 +0900

    [level 1] Title: 옹알이 (2), Time: 0.13 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,27 +1,24 @@
 def solution(babbling):
     ans = 0
-    
-    for bab in babbling:
+    for b in babbling:
+        ok = True
         pre = ''
-        ok = True    
         
-        while(bab):
-            s2 = bab[:2]
-            s3 = bab[:3]
+        while (b):
+            two = b[:2]
+            the = b[:3]
             
-            if pre == s2 or pre == s3:
-                ok = False
-                break
-            
-            if s2 == 'ye' or s2 == 'ma': 
-                pre = s2
-                bab = bab[2:]
-            elif s3 == 'aya' or s3 == 'woo':
-                pre = s3
-                bab = bab[3:]
+            if two in ["ye", "ma"] and two != pre:
+                pre = two
+                b = b[2:]
+            elif the in ["aya", "woo"] and the != pre:
+                pre = the
+                b = b[3:]
             else:
                 ok = False
                 break
+
         if ok:
             ans += 1
+    
     return ans
